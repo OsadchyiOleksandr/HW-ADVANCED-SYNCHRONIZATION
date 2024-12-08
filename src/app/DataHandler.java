168a97f9ef616a536cb19ee13b5d6af80d5bdce1
Oleksandr Osadchyi <o.osadchyi@adtelligent.com>
2024-12-08 11:36:34 +0200

    Fixed extra synchronized and changed ReentrantLock to Lock

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,12 +1,13 @@
 package app;
 
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DataHandler {
 
-    private final ReentrantLock lock = new ReentrantLock();
+    private final Lock lock = new ReentrantLock();
 
-    public synchronized int modify(int num) {
+    public int modify(int num) {
         lock.lock();
         try {
             num = num * 3;
